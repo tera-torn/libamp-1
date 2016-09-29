33d7640185fdf9b08031bb2c3f02176c6af0bfc8
teratorn <teratorn /AT/ zoho /DOT/ com>
2016-09-28 23:51:11 -0700

    clean up benchmark running; implement new benchmark using the bench_client program

--- examples/run_benchmarks.py
+++ examples/run_benchmarks.py
@@ -9,16 +9,16 @@ import time
 
 #locale.setlocale(locale.LC_ALL, 'en_US')
 
-if len(sys.argv) != 2:
-    raise ValueError("Pass number of boxes to parse as first argument.")
-
-count = int(sys.argv[1])
+if len(sys.argv) < 2:
+    count = 4000000
+else:
+    count = int(sys.argv[1])
 
 env = os.environ.copy()
 env['LD_LIBRARY_PATH'] = '..'
 env['DYLD_LIBRARY_PATH'] = '..'
 
-def runBenchmark(executableName):
+def runBenchmark(executableName, msg):
     then = time.time()
     p = Popen(['./'+executableName, str(count)], stdout=PIPE, stderr=PIPE, env=env)
     out, err = p.communicate()
@@ -28,10 +28,48 @@ def runBenchmark(executableName):
         print '%s error. returncode: %d stderr: %s' % (executableName, p.returncode, out+err)
         sys.exit(1)
 
-    print('%s: parsed %s AMP boxes in %.4f seconds (%s boxes/s)' % 
-          (executableName, locale.format("%d", count, grouping=True),
+    print('%s %s: parsed %s AMP boxes in %.4f seconds (%s boxes/s)' %
+          (executableName, msg, locale.format("%d", count, grouping=True),
            t, locale.format("%d", int(count/t), grouping=True)))
 
-for name in ('bench_parsing', 'bench_parsing_new'):
-    runBenchmark(name) 
+for name, msg in (('bench_parsing',     '(current implementation)'),
+                  ('bench_parsing_new', '(in testing - unused)')):
+    runBenchmark(name, msg)
     print
+
+
+def runLocalhostBenchmark():
+    serverName = 'async_server'
+    serverPort = '22380'
+    clientName = 'bench_client'
+    hostName   = 'localhost'
+    localhostCount = count / 50
+    p1 = Popen(['./'+serverName, serverPort], stdout=PIPE, stderr=PIPE, env=env)
+
+    then = time.time()
+    p2 = Popen(['./'+clientName, hostName+':'+serverPort, str(localhostCount)], stdout=PIPE, stderr=PIPE, env=env)
+    out2, err2 = p2.communicate()
+    t = time.time() - then
+
+    p1.kill()
+    out1, err1 = p1.communicate()
+
+    if p2.returncode != 0:
+        print '%s error. returncode: %d' % (executableName, p2.returncode)
+        print clientName, 'stdout:'
+        print out2
+        print clientName, 'stderr:'
+        print err2
+
+        print serverName, 'stdout:'
+        print out1
+        print serverName, 'stderr:'
+        print err1
+        sys.exit(1)
+
+    print('%s: Made %s AMP calls (back-to-back in series - over '
+          'localhost TCP socket) in %.4f seconds (%s calls/s)' %
+          (clientName, locale.format("%d", localhostCount, grouping=True),
+           t, locale.format("%d", int(localhostCount/t), grouping=True)))
+
+runLocalhostBenchmark()
